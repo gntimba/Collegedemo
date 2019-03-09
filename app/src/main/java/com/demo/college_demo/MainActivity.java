55b90c5f2f9a88a2567373dac4cff068116550d0
godfrey Timba <gntimba@gmail.com>
2019-03-09 02:09:57 +0200

    finished step1

--- app/src/main/java/com/demo/college_demo/MainActivity.java
+++ app/src/main/java/com/demo/college_demo/MainActivity.java
@@ -1,13 +1,16 @@
 package com.demo.college_demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      //  getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        //getActionBar().hide();
     }
 }
