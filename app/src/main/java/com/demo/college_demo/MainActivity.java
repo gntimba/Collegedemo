5d1a8e619aedb235d5a843f98f78f416d156d17e
godfrey Timba <gntimba@gmail.com>
2019-03-09 03:15:44 +0200

    few changes n click events

--- app/src/main/java/com/demo/college_demo/MainActivity.java
+++ app/src/main/java/com/demo/college_demo/MainActivity.java
@@ -2,15 +2,60 @@ package com.demo.college_demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    GridLayout mainGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      //  getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
-        //getActionBar().hide();
+
+        mainGrid =findViewById(R.id.grid);
+        //set event
+    //setSingleEvent(mainGrid);
     }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        //loop all items of main grid
+
+    }
+    public void timeTable (View view)
+    {
+        Toast.makeText(this, "timeTable", Toast.LENGTH_SHORT).show();
+    }
+    public void attendance (View view)
+    {
+        Toast.makeText(this, "attendance", Toast.LENGTH_SHORT).show();
+    }
+    public void chat (View view)
+    {
+        Toast.makeText(this, "chat", Toast.LENGTH_SHORT).show();
+    }
+    public void query (View view)
+    {
+        Toast.makeText(this, "query", Toast.LENGTH_SHORT).show();
+    }
+    public void upload (View view)
+    {
+        Toast.makeText(this, "upload", Toast.LENGTH_SHORT).show();
+    }
+    public void finance (View view)
+    {
+        Toast.makeText(this, "finance", Toast.LENGTH_SHORT).show();
+    }
+    public void academic (View view)
+    {
+        Toast.makeText(this, "academic", Toast.LENGTH_SHORT).show();
+    }
+    public void libray (View view)
+    {
+        Toast.makeText(this, "libray", Toast.LENGTH_SHORT).show();
+    }
+
 }
