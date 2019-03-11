a744a0b18b70f30ad47f2d2091980444384f68a4
gntimba <gntimba@gmail.com>
2019-03-11 15:51:56 +0200

    time table

---
+++ app/src/main/java/com/demo/college_demo/timeTable.java
@@ -0,0 +1,13 @@
+package com.demo.college_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class timeTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_table);
+    }
+}
